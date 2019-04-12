82d3279b5b4dd46514a149c1596edb22f215faf9
mattman059 <mattman059@gmail.com>
2019-04-11 20:18:46 -0500

    Update myCherry.py

--- autoCherryTree/myCherry.py
+++ autoCherryTree/myCherry.py
@@ -19,7 +19,7 @@ def header(handle):
 
 def pingScan(handle, targets):
 
-    handle.write("<node custom_icon_id='0' is_bold='True' name='Hosts' prog_lang='custom-colors' >\n")
+    handle.write("<node custom_icon_id='0' is_bold='True' name='Hosts' prog_lang='custom-colors' >\n") #START HOSTS  NODE
     SWEEP = "nmap -n -sP %s" % (targets)
     results = subprocess.check_output(SWEEP, shell=True)
     lines = str(results).encode("utf-8").split("\n")
@@ -27,10 +27,10 @@ def pingScan(handle, targets):
 
     #Create a new page for each live host
     for curIP in liveHosts:
-        handle.write("<node custom_icon_id='0' is_bold='False' name='" + curIP + "' prog_lang='custom-colors' >\n")
+        handle.write("<node custom_icon_id='0' is_bold='False' name='" + curIP + "' prog_lang='custom-colors' >\n") #START IP NODE
         #tcp scan
-        handle.write("<node custom_icon_id='0' is_bold='False' name='TCP Service Scan' prog_lang='custom-colors'>\n")
-        handle.write("<rich_text>\n")
+        handle.write("<node custom_icon_id='0' is_bold='False' name='TCP Service Scan' prog_lang='custom-colors'>\n") #START TCP NODE
+        handle.write("<rich_text>\n") #START TCP NODE TEXT
         SCAN = "nmap -sC -sV -vvv %s" % (curIP)
         print("[SCAN] " + SCAN)
         SCAN_result = subprocess.check_output(SCAN, shell=True)
@@ -43,11 +43,11 @@ def pingScan(handle, targets):
             print(" NMAP LIST RESULT")
             print("[DEBUG] " + str(SCAN_lines))
         handle.write(SCAN_result)
-        handle.write("</rich_text>\n")
-        handle.write("</node>\n")
-        handle.write("</node>\n")
+        handle.write("</rich_text>\n") #END TCP NODE TEXT
+        handle.write("</node>\n") #END TCP NODE
+        handle.write("</node>\n") #END IP NODE
         
-    handle.write("</node>\n")
+    handle.write("</node>\n") #END HOSTS NODE
 
 
 def footer(handle):
