d3ad343290dddfe410b837b2eed023d1c92d2a76
mattman059 <mattman059@gmail.com>
2019-06-08 19:39:21 -0500

    Adding subnet cheat
    Updated some files

---
+++ subnet_Cheat.py
@@ -0,0 +1,11 @@
+import ipaddress
+import sys
+
+
+def main():
+    for arg in sys.argv[1:]:
+        addr = ipaddress.ip_interface(arg)
+        print("IP Address: ", addr)
+        print("Network: ", addr.network)
+        print("Network Mask: ", addr.netmask)
+        print("Broadcast Address", addr.network.broadcast_address)
