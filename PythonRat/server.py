4a4fe8a6a0f94c9d80412c0ab3dda735bc22d3d4
mattman059 <mattman059@gmail.com>
2022-03-02 19:45:35 -0600

    Update server.py

--- PythonRat/server.py
+++ PythonRat/server.py
@@ -1,6 +1,8 @@
 from server_class import *
         
 def main():
+    #Create a server on the desired interface.
+    #The server will start, and the menu will be presented
     s = Server("10.0.0.60",12345)
       
 if __name__ == "__main__":
