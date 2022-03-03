05027ac7b0f34626148430e473df12ebfbf4d1f2
mattman059 <mattman059@gmail.com>
2022-03-02 19:46:25 -0600

    Update capabilities.py

--- PythonRat/capabilities.py
+++ PythonRat/capabilities.py
@@ -1,5 +1,19 @@
 import subprocess
 
 def run(command):
+    """
+    Parameters
+    ----------
+    command : str
+        The command to execute via subprocess pipe
+
+
+    Returns
+    -------
+    str
+        stdout/stderr result of the executed command
+    """
+
+    
     output = subprocess.Popen(command, shell=True,stdout=subprocess.PIPE, stderr=subprocess.PIPE,stdin=subprocess.PIPE)
     return output.stdout.read().decode() + output.stderr.read().decode()
